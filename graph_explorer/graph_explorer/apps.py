117f9a9b7003a88c01857b739aa30e8788335476
David Makan <davemakan@gmail.com>
2025-09-16 14:29:24 +0200

    #1 add base html page with css and adjust some configurations

--- graph_explorer/graph_explorer/apps.py
+++ graph_explorer/graph_explorer/apps.py
@@ -1,6 +1,20 @@
 from django.apps import AppConfig
+import pkg_resources
+
+from api.models.graph import Graph
 
 
-class CoreConfig(AppConfig):
+class GraphExplorerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'graph_explorer'
+    label = 'graph_explorer'
+    visualization_plugins = []
+    current_graph = Graph([], [])
+
+    def ready(self):
+        print("Visualizer plugins:")
+        for ep in pkg_resources.iter_entry_points(group='visualizer'):
+            p = ep.load()
+            print("{} {}".format(ep.name, p))
+            plugin = p()
+            self.visualization_plugins.append(plugin)
--- graph_explorer/graph_explorer/views.py
+++ graph_explorer/graph_explorer/views.py
@@ -1,3 +1,35 @@
+from django.apps.registry import apps
+from django.http import HttpRequest
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+from api.models.graph import Graph
+
+def index(request: HttpRequest):
+    visualization_plugins = apps.get_app_config('graph_explorer').visualization_plugins
+    g = Graph([], [])
+    g.add_node(0, {'a': 23, 'b': 56})
+    g.add_node(1, {'a': 65, 'b': 47})
+    g.add_node(2, {'a': 54, 'b': 45})
+    g.add_node(3, {'a': 21, 'b': 21})
+    g.add_node(4, {'a': 69, 'b': 56})
+    g.add_node(5, {'a': 99, 'b': 96})
+    g.add_node(6, {'a': 100, 'b': 56})
+    g.add_edge(0, 1, 2, None)
+    g.add_edge(1, 1, 4, None)
+    g.add_edge(2, 1, 3, None)
+    g.add_edge(3, 2, 4, None)
+    g.add_edge(4, 3, 2, None)
+    g.add_edge(5, 3, 6, None)
+    g.add_edge(6, 3, 5, None)
+    g.add_edge(7, 4, 0, None)
+    apps.get_app_config('graph_explorer').current_graph = g
+    return render(request, "index.html", {"visualization_plugins": visualization_plugins})
+
+
+@csrf_exempt
+def visualize(request: HttpRequest):
+    visualization_plugins = apps.get_app_config('graph_explorer').visualization_plugins
+    if request.method == 'POST':
+        print(request.POST)
+    return render(request, "index.html", {"visualization_plugins": visualization_plugins})
