7851241f6af25a374d12cbb225783d65db56d942
David Makan <davemakan@gmail.com>
2025-09-21 13:52:09 +0200

    #11: add filters

--- api/api/services/search_filter.py
+++ api/api/services/search_filter.py
@@ -17,3 +17,27 @@ def search(g: Graph, text: str) -> Graph:
         result.add_link(link.id, link.source, link.target)
     
     return result
+
+def filter(g: Graph, attr: str, op: str, val: str) -> Graph:
+    if op not in ['==', '<=', '>=', '<', '>', '!=']:
+        return g
+    if attr is None or attr == "" or val is None or val == "":
+        return g
+    result = Graph([], [])
+    for node in g.nodes:
+        if attr not in node.attributes:
+            continue
+
+        attr_val = node.attributes[attr]
+        value = val
+        if not (isinstance(attr_val, float) or isinstance(attr_val, int)):
+            attr_val = f"'{str(node.attributes[attr]).lower()}'"
+            value = f"'{val.lower()}'"
+        
+        if eval(f"{str(attr_val)}{op}{value}"):
+            result.add_node(node.id, node.attributes)
+
+    for link in g.links:
+        result.add_link(link.id, link.source, link.target)
+    
+    return result
