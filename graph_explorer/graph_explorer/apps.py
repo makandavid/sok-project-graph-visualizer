59e31584fc26c4a7acf3a431e08ccf7320e6b0fe
nikolinapetronic <petronic.sv59.2022@uns.ac.rs>
2025-09-24 19:58:30 +0200

    #15 refactor existing code to to use the Workspace model and WorkspaceService

--- graph_explorer/graph_explorer/apps.py
+++ graph_explorer/graph_explorer/apps.py
@@ -3,6 +3,7 @@ from django.apps import AppConfig
 
 from core.use_cases.const import VISUALIZER_GROUP, DATASOURCE_GROUP
 from core.use_cases.plugin_recognition import PluginService
+from core.use_cases.workspace_management import WorkspaceService
 
 
 class GraphExplorerConfig(AppConfig):
@@ -10,6 +11,7 @@ class GraphExplorerConfig(AppConfig):
     label = 'graph_explorer'
 
     plugin_service: PluginService
+    workspace_service: WorkspaceService
     current_visualization_plugin = None
     current_graph = Graph([], [])
     filtered_graph = Graph([], [])
@@ -17,5 +19,6 @@ class GraphExplorerConfig(AppConfig):
 
     def ready(self):
         self.plugin_service = PluginService()
+        self.workspace_service = WorkspaceService()
         self.plugin_service.load_plugins(VISUALIZER_GROUP)
         self.plugin_service.load_plugins(DATASOURCE_GROUP)
